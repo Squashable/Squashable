fba426a0bf536857db5ea6d181c7df7dd3b06041
Squashable <josh@hudgell.uk>
2025-06-28 12:14:26 +0100

    home page & project links
    added links to projects 4 through 10.
    Home page now has more content and fixed boxes.
    Not sure what to use the boxes for yet

--- navbar-loader.js
+++ navbar-loader.js
@@ -9,7 +9,14 @@ fetch("navbar.html")
       "Artifical.html": "My Stance on AI Use",
       "website-turret.html": "Turret",
       "website-model-rail.html": "Model Rail",
-      "website-project.html": "Website Building"
+      "website-project.html": "Website Building",
+      "Project04.html": "Future Project 04",
+      "Project05.html": "Future Project 05",
+      "Project06.html": "Future Project 06",
+      "Project07.html": "Future Project 07",
+      "Project08.html": "Future Project 08",
+      "Project09.html": "Future Project 09",
+      "Project10.html": "Future Project 10"
     };
     const currentPage = window.location.pathname.split("/").pop();
     const title = pageMap[currentPage] || "Portfolio";
